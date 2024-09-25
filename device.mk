b4936b7bb09763affadf3baf421c6015acbbbeda
Michael Bestas <mkbestas@lineageos.org>
2024-09-26 01:39:46 +0300

    miatoll: Switch to AIDL USB HALs
    Change-Id: I70f6a4e57eef577d86987bfae44f381a4f107624
    
    Signed-off-by: IHSAN <ihsanulrahman@gmail.com>

--- device.mk
+++ device.mk
@@ -471,8 +471,8 @@ AB_OTA_UPDATER := false
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti \
-    android.hardware.usb.gadget@1.2-service-qti
+    android.hardware.usb-service.qti \
+    android.hardware.usb.gadget-service.qti
 
 PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
